1cd447264f38e8fb5a1f2c4c01d919606b1b4a40
shubham <shubham0209@gmail.com>
2017-08-23 19:33:01 +0530

    Added host

--- thirdauth/settings.py
+++ thirdauth/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9n#wl-5%y=piso#z67^%@eswcbcp%*hp#ij*_2@8e54+#1@^2v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shubham0209.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
